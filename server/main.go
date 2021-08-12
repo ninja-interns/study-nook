ecd3e91131d22c4fca86e2bda8e2d92b0f0e93b3
Joao Soares <62054293+JoaoSoaresDev@users.noreply.github.com>
2021-08-12 13:04:42 +0800

    Adding server folder

---
+++ server/main.go
@@ -0,0 +1,14 @@
+// Main file of study nook
+
+// When creating new files for your React components or pages,
+// create a subdirectory on this directory and name it with
+// whichever feature you are creating.
+
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Welcome to study nook! :)")
+}
