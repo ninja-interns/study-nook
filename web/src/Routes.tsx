2d6b917c90df333c827959ded3e069d34562e374
AlyssaThi <aruefenacht14@gmail.com>
2021-08-16 16:02:35 +0800

    FE Login

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
-import { HomePage, RegistrationPage } from './pages';
-import { createMemoryHistory } from 'history';
-import { Registration } from './components';
+import React from "react";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { HomePage, RegistrationPage, LoginPage } from "./pages";
+import { createMemoryHistory } from "history";
 
 const Routes = () => {
+	//const history = createMemoryHistory();
 
-    const history = createMemoryHistory();
-
-    return (
-        <Router>
-            <Switch>
-                <Route path="/registration" component={RegistrationPage} />
-                <Route path="/" component={HomePage} />
-            </Switch>
-        </Router>
-    )
-}
+	return (
+		<Router>
+			<Switch>
+				<Route path="/registration" component={RegistrationPage} />
+				<Route path="/login" component={LoginPage} />
+				<Route exact path="/" component={HomePage} />
+			</Switch>
+		</Router>
+	);
+};
 
 export default Routes;
