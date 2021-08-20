5bee78ce67713c1d462488e7f93e31d6d7b64cea
AlyssaThi <aruefenacht14@gmail.com>
2021-08-20 16:15:24 +0800

    logout function destroys the session cookie

--- server/main.go
+++ server/main.go
@@ -9,8 +9,6 @@ package main
 import (
 	"net/http"
 
-	"github.com/alexedwards/scs/postgresstore"
-	"github.com/alexedwards/scs/v2"
 	"github.com/go-chi/chi/v5"
 	"main.go/auth"
 	initializeDB "main.go/initializedb"
@@ -19,13 +17,13 @@ import (
 func main() {
 	initializeDB.InitDB()
 
-	auth.SessionManager = scs.New()
-	auth.SessionManager.Store = postgresstore.New(initializeDB.Db)
+	auth.SessionsConfig()
 
 	r := chi.NewRouter()
 
 	r.HandleFunc("/api/createUser", auth.CreateUser)
 	r.HandleFunc("/api/loginUser", auth.LoginUser)
+	r.HandleFunc("/api/logoutUser", auth.LogoutUser)
 
 	http.ListenAndServe(":8080", auth.SessionManager.LoadAndSave(r))
 }
