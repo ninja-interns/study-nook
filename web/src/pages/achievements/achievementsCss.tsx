41045a3d3ff7d808835222cd34827e174c71d981
Joao Soares <62054293+JoaoSoaresDev@users.noreply.github.com>
2021-09-06 15:03:51 +0800

    Page implemented, missing some design improvements

--- web/src/pages/achievements/achievementsCss.tsx
+++ web/src/pages/achievements/achievementsCss.tsx
@@ -7,7 +7,12 @@ export const useStyles = makeStyles({
         height: "600px",
         position: "absolute",
         border: "solid black",
+        overflowY: "scroll",
+        '&::-webkit-scrollbar': {
+            display: "none",
+        },
     },
+
     closeButton: {
         width: "40px",
         position: "absolute",
@@ -16,5 +21,50 @@ export const useStyles = makeStyles({
         margin: "20px",
         cursor: "pointer",
     },
+    badgesTable: {
+        margin: "40px",
+        marginTop: "80px",
+        display: "grid",
+        gridTemplateColumns: "auto auto auto",
+    },
+    badgeElement: {
+        width: "50px",
+        margin: "20px",
+        marginBottom: "10px",
+        cursor: "pointer",
+        '&:hover': {
+            "& ~ $textBox": {
+                display: "block",
+                position: "absolute",
+            }
+        }
+    },
+    badgeCaption: {
+        textAlign: "center",
+        marginRight: "5px",
+        marginBottom: "20px",
+        cursor: "pointer",
+        '&:hover': {
+            "& + $textBox": {
+                display: "block",
+            }
+        }
+    },
+    textBox: {
+        background: "#F8F8F8",
+        border: "5px solid #DFDFDF",
+        color: "#717171",
+        fontSize: "13px",
+        height: "40px",
+        width: "310px",
+        letterSpacing: "1px",
+        lineHeight: "20px",
+        position: "absolute",
+        textAlign: "center",
+        textTransform: "uppercase",
+        display: "none",
+        padding: "0 0px",
+        overflow: "auto"
+    }
 
 });
--- web/src/pages/achievements/index.tsx
+++ web/src/pages/achievements/index.tsx
@@ -1,8 +1,9 @@
-import { Slide } from "@material-ui/core";
+import { Slide, Fade } from "@material-ui/core";
 import { useStyles } from "./achievementsCss";
 import { useHistory } from "react-router-dom";
 
 import closeButton from "../../assets/close-button.png";
+import badgeIcon from "../../assets/medal.png";
 
 export function AchievementsPage() {
     const css = useStyles();
@@ -13,8 +14,81 @@ export function AchievementsPage() {
             <div className={css.container}>
                 <img className={css.closeButton} src={closeButton} alt="Close button" onClick={() => history.goBack()}></img>
 
+                <Fade in={true} timeout={2000}>
+                    <div className={css.badgesTable}>
+                        <figure>
+                            <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"level-1"} />
+                            <figcaption className={css.badgeCaption}>Level badge - Level 1</figcaption>
+                            <span className={css.textBox}> Some text </span>
+                        </figure>
 
+                        <figure>
+                            <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"level-20"} />
+                            <figcaption className={css.badgeCaption} >Level badge - Level 20</figcaption>
+                            <span className={css.textBox}> Some text </span>
+                        </figure>
 
+                        <figure>
+                            <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"level-50"} />
+                            <figcaption className={css.badgeCaption}>Level badge - Level 50</figcaption>
+                            <span className={css.textBox}> Some text </span>
+                        </figure>
+
+                        <figure>
+                            <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"sessions-completed-1"} />
+                            <figcaption className={css.badgeCaption}>Sessions completed - 1 Session</figcaption>
+                            <span className={css.textBox}> Some text </span>
+                        </figure>
+
+                        <figure>
+                            <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"sessions-completed-10"} />
+                            <figcaption className={css.badgeCaption}>Sessions completed - 10 session</figcaption>
+                            <span className={css.textBox}> Some text </span>
+                        </figure>
+
+                        <figure>
+                            <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"sessions-completed-20"} />
+                            <figcaption className={css.badgeCaption}>Sessions completed - 20 sessions</figcaption>
+                            <span className={css.textBox}> Some text </span>
+                        </figure>
+
+                        <figure>
+                            <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"hours-nooking-1"} />
+                            <figcaption className={css.badgeCaption}>Hours focused - 1 hour</figcaption>
+                            <span className={css.textBox}> Some text </span>
+                        </figure>
+
+                        <figure>
+                            <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"hours-nooking-5"} />
+                            <figcaption className={css.badgeCaption}>Hours focused - 5 hours</figcaption>
+                            <span className={css.textBox}> Some text </span>
+                        </figure>
+
+                        <figure>
+                            <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"hours-nooking-10"} />
+                            <figcaption className={css.badgeCaption}>Hours focused - 10 hours</figcaption>
+                            <span className={css.textBox}> Some text </span>
+                        </figure>
+
+                        <figure>
+                            <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"hours-nooking-1"} />
+                            <figcaption className={css.badgeCaption}>Characters unlocked - 2</figcaption>
+                            <span className={css.textBox}> Some text </span>
+                        </figure>
+
+                        <figure>
+                            <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"hours-nooking-5"} />
+                            <figcaption className={css.badgeCaption}>Characters unlocked - 5</figcaption>
+                            <span className={css.textBox}> Some text </span>
+                        </figure>
+
+                        <figure>
+                            <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"hours-nooking-10"} />
+                            <figcaption className={css.badgeCaption}>Characters unlocked - 10</figcaption>
+                            <span className={css.textBox}> Some text </span>
+                        </figure>
+                    </div>
+                </Fade>
 
             </div >
         </Slide>
