bab95834b3632e665677fffc073e7508f2c733fe
Alicia <leishbiggs@live.com.au>
2021-09-08 13:22:51 +0800

    issues after merge fixed

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,18 @@
-import React from "react";
-import { ContextContainer } from "./contexts/ContextContainer";
-import Routes from "./routes/Routes";
-import { ThemeProvider } from "@material-ui/styles";
-import { theme } from "./contexts/themeContext";
+import React from "react"
+import { ContextContainer } from "./contexts/ContextContainer"
+import Routes from "./routes/Routes"
+import { ThemeProvider } from "@material-ui/styles"
+import { theme } from "./contexts/themeContext"
+import TodoListApp from "./components/todoList"
 
 function App() {
-	return (
-		<ThemeProvider theme={theme}>
-			<ContextContainer.Provider>
-				<Routes />
-			</ContextContainer.Provider>
-		</ThemeProvider>
-	);
+    return (
+        <ThemeProvider theme={theme}>
+            <ContextContainer.Provider>
+                {/* <Routes /> */}
+                <TodoListApp />
+            </ContextContainer.Provider>
+        </ThemeProvider>
+    )
 }
 export default App
