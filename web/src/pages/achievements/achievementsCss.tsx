1749666997764e8ab2d92d4d17d673c4c3f91286
Joao Soares <62054293+JoaoSoaresDev@users.noreply.github.com>
2021-09-08 10:11:24 +0800

    Front-end page full implemented. Missing design improvement and API calls

--- web/src/pages/achievements/achievementsCss.tsx
+++ web/src/pages/achievements/achievementsCss.tsx
@@ -42,23 +42,45 @@ export const useStyles = makeStyles({
         marginRight: "5px",
         marginBottom: "20px",
         cursor: "pointer",
+        '&:hover': {
+            "& + $textBox": {
+                display: "block",
+            }
+        }
     },
     textBox: {
         margin: "-70px",
         marginLeft: "-30px",
         background: "#F8F8F8",
-        border: "5px solid #DFDFDF",
+        border: "1px solid black",
         color: "#717171",
-        fontSize: "13px",
+        fontSize: "10px",
         height: "40px",
-        width: "150px",
+        width: "160px",
         letterSpacing: "1px",
         lineHeight: "20px",
         position: "absolute",
-        textAlign: "center",
+        textAlign: "left",
         textTransform: "uppercase",
         display: "none",
-        padding: "0 0px",
+        padding: "3px",
+        paddingLeft: "8px",
+        paddingRight: "8px",
         pointerEvents: "none",
     },
+    bar: {
+        background: "#353b48",
+        display: "block",
+        height: "7px",
+        border: "1px solid rgba(0,0,0, 0.3)",
+        width: "140px",
+        borderRadius: "10%",
+        overflow: "hidden",
+    },
+    achievementLevel1: {
+
+    },
+    tracker: {
+        float: "right",
+    }
 });
--- web/src/pages/achievements/index.tsx
+++ web/src/pages/achievements/index.tsx
@@ -19,73 +19,133 @@ export function AchievementsPage() {
                         <figure>
                             <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"level-1"} />
                             <figcaption className={css.badgeCaption}>Level badge - Level 1</figcaption>
-                            <span className={css.textBox}> Some text </span>
+                            <span className={css.textBox}> Progression:
+                                <p className={css.tracker}>0/1</p>
+                                <span className={css.bar}>
+                                    <span className={css.achievementLevel1}></span>
+                                </span>
+                            </span>
                         </figure>
 
                         <figure>
                             <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"level-20"} />
-                            <figcaption className={css.badgeCaption} >Level badge - Level 20</figcaption>
-                            <span className={css.textBox}> Some text </span>
+                            <figcaption className={css.badgeCaption} >Level badge - Level 10</figcaption>
+                            <span className={css.textBox}> Progression:
+                                <p className={css.tracker}>0/10</p>
+                                <span className={css.bar}>
+                                    <span className={css.achievementLevel1}></span>
+                                </span>
+                            </span>
                         </figure>
 
                         <figure>
                             <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"level-50"} />
-                            <figcaption className={css.badgeCaption}>Level badge - Level 50</figcaption>
-                            <span className={css.textBox}> Some text </span>
+                            <figcaption className={css.badgeCaption}>Level badge - Level 20</figcaption>
+                            <span className={css.textBox}> Progression:
+                                <p className={css.tracker}>0/20</p>
+                                <span className={css.bar}>
+                                    <span className={css.achievementLevel1}></span>
+                                </span>
+                            </span>
                         </figure>
 
                         <figure>
                             <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"sessions-completed-1"} />
                             <figcaption className={css.badgeCaption}>Sessions completed - 1 Session</figcaption>
-                            <span className={css.textBox}> Some text </span>
+                            <span className={css.textBox}> Progression:
+                                <p className={css.tracker}>0/1</p>
+                                <span className={css.bar}>
+                                    <span className={css.achievementLevel1}></span>
+                                </span>
+                            </span>
                         </figure>
 
                         <figure>
                             <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"sessions-completed-10"} />
                             <figcaption className={css.badgeCaption}>Sessions completed - 10 session</figcaption>
-                            <span className={css.textBox}> Some text </span>
+                            <span className={css.textBox}> Progression:
+                                <p className={css.tracker}>0/10</p>
+                                <span className={css.bar}>
+                                    <span className={css.achievementLevel1}></span>
+                                </span>
+                            </span>
                         </figure>
 
                         <figure>
                             <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"sessions-completed-20"} />
                             <figcaption className={css.badgeCaption}>Sessions completed - 20 sessions</figcaption>
-                            <span className={css.textBox}> Some text </span>
+                            <span className={css.textBox}> Progression:
+                                <p className={css.tracker}>0/20</p>
+                                <span className={css.bar}>
+                                    <span className={css.achievementLevel1}></span>
+                                </span>
+                            </span>
                         </figure>
 
                         <figure>
                             <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"hours-nooking-1"} />
                             <figcaption className={css.badgeCaption}>Hours focused - 1 hour</figcaption>
-                            <span className={css.textBox}> Some text </span>
+                            <span className={css.textBox}> Progression:
+                                <p className={css.tracker}>0/1</p>
+                                <span className={css.bar}>
+                                    <span className={css.achievementLevel1}></span>
+                                </span>
+                            </span>
                         </figure>
 
                         <figure>
                             <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"hours-nooking-5"} />
                             <figcaption className={css.badgeCaption}>Hours focused - 5 hours</figcaption>
-                            <span className={css.textBox}> Some text </span>
+                            <span className={css.textBox}> Progression:
+                                <p className={css.tracker}>0/5</p>
+                                <span className={css.bar}>
+                                    <span className={css.achievementLevel1}></span>
+                                </span>
+                            </span>
                         </figure>
 
                         <figure>
                             <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"hours-nooking-10"} />
                             <figcaption className={css.badgeCaption}>Hours focused - 10 hours</figcaption>
-                            <span className={css.textBox}> Some text </span>
+                            <span className={css.textBox}> Progression:
+                                <p className={css.tracker}>0/10</p>
+                                <span className={css.bar}>
+                                    <span className={css.achievementLevel1}></span>
+                                </span>
+                            </span>
                         </figure>
 
                         <figure>
                             <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"hours-nooking-1"} />
                             <figcaption className={css.badgeCaption}>Characters unlocked - 2</figcaption>
-                            <span className={css.textBox}> Some text </span>
+                            <span className={css.textBox}> Progression:
+                                <p className={css.tracker}>0/2</p>
+                                <span className={css.bar}>
+                                    <span className={css.achievementLevel1}></span>
+                                </span>
+                            </span>
                         </figure>
 
                         <figure>
                             <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"hours-nooking-5"} />
                             <figcaption className={css.badgeCaption}>Characters unlocked - 5</figcaption>
-                            <span className={css.textBox}> Some text </span>
+                            <span className={css.textBox}> Progression:
+                                <p className={css.tracker}>0/5</p>
+                                <span className={css.bar}>
+                                    <span className={css.achievementLevel1}></span>
+                                </span>
+                            </span>
                         </figure>
 
                         <figure>
                             <img className={css.badgeElement} src={badgeIcon} alt="Badge" id={"hours-nooking-10"} />
                             <figcaption className={css.badgeCaption}>Characters unlocked - 10</figcaption>
-                            <span className={css.textBox}> Some text </span>
+                            <span className={css.textBox}> Progression:
+                                <p className={css.tracker}>0/10</p>
+                                <span className={css.bar}>
+                                    <span className={css.achievementLevel1}></span>
+                                </span>
+                            </span>
                         </figure>
                     </div>
                 </Fade>
