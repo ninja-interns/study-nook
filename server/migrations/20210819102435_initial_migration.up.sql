fb329971b2e9f5d26a6e2c974c8d6454d159507c
Alyssa ROG-Linux <aruefenacht14@gmail.com>
2021-09-16 15:43:36 +0800

    unique shopitemsowned db constraint

--- server/migrations/20210819102435_initial_migration.up.sql
+++ server/migrations/20210819102435_initial_migration.up.sql
@@ -1,42 +1,48 @@
-CREATE TABLE IF NOT EXISTS users(
-    id TEXT PRIMARY KEY,
-    email TEXT UNIQUE NOT NULL,
-    password_hash BYTEA NOT NULL,
-    name TEXT NOT NULL,
-    username TEXT UNIQUE NOT NULL,
-    is_verified BOOLEAN NOT NULL,
-    token TEXT
+CREATE TABLE IF NOT EXISTS users (
+    id text PRIMARY KEY,
+    email text UNIQUE NOT NULL,
+    password_hash bytea NOT NULL,
+    name text NOT NULL,
+    username text UNIQUE NOT NULL,
+    is_verified boolean NOT NULL,
+    token text
 );
 
 CREATE TABLE IF NOT EXISTS sessions (
-	token TEXT PRIMARY KEY,
-	data BYTEA NOT NULL,
-	expiry TIMESTAMPTZ NOT NULL
+    token text PRIMARY KEY,
+    data bytea NOT NULL,
+    expiry timestamptz NOT NULL
 );
 
 CREATE INDEX IF NOT EXISTS sessions_expiry_idx ON sessions (expiry);
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
- 
-CREATE TABLE IF NOT EXISTS shopItems(
-    id UUID NOT NULL DEFAULT uuid_generate_v4(),
+
+CREATE TABLE IF NOT EXISTS shopItems (
+    id uuid NOT NULL DEFAULT uuid_generate_v4 (),
     CONSTRAINT pkey PRIMARY KEY (id),
-    category TEXT NOT NULL,
-    name TEXT NOT NULL,
+    category text NOT NULL,
+    name text NOT NULL,
     level INT NOT NULL,
     cost INT NOT NULL,
-    src TEXT
+    src text
 );
 
-INSERT INTO shopItems(category, name, level, cost, src) VALUES('backgrounds', 'Cool Orange', 3, 200, 'exampleBackground.jpg');
-INSERT INTO shopItems(category, name, level, cost, src) VALUES('backgrounds', 'Pastels', 3, 200, 'pastelBookShelf.jpg');
+INSERT INTO shopItems (category, name, level,
+    COST, src)
+    VALUES ('backgrounds', 'Cool Orange', 3, 200, 'exampleBackground.jpg');
 
-CREATE TABLE IF NOT EXISTS shopItemsOwned(
-    id UUID NOT NULL DEFAULT uuid_generate_v4(),
-    shopItemID UUID REFERENCES shopItems(id),
-    userID TEXT REFERENCES users(id),
-    category TEXT NOT NULL,
-    name TEXT NOT NULL,
+INSERT INTO shopItems (category, name, level,
+    COST, src)
+    VALUES ('backgrounds', 'Pastels', 3, 200, 'pastelBookShelf.jpg');
+
+CREATE TABLE IF NOT EXISTS shopItemsOwned (
+    id uuid NOT NULL DEFAULT uuid_generate_v4 (),
+    shopItemID uuid UNIQUE REFERENCES shopItems (id),
+    userID text REFERENCES users (id),
+    category text NOT NULL,
+    name text NOT NULL,
     level INT NOT NULL,
-    src TEXT
+    src text
 );
+
