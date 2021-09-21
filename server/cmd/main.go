99029babf0f5af78cae419b9f6a80171ec38dbe4
Alyssa Ninja Linux <aruefenacht14@gmail.com>
2021-09-21 12:13:05 +0800

    idk what happened but emailer works again

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/urfave/cli/v2"
 	"studynook.go/api"
 	"studynook.go/db"
+	"studynook.go/emails"
 )
 
 func main() {
@@ -22,11 +23,50 @@ func main() {
 				Usage:   "run the server on certain port [default: 8080]",
 				Aliases: []string{"s"},
 				Flags: []cli.Flag{
-					&cli.StringFlag{Name: "address", Aliases: []string{"a"}, Value: ":8080", Usage: "Set the address to use for the server"},
-					&cli.StringFlag{Name: "user", Aliases: []string{"u"}, Value: "dev", Usage: "Set the database username in the database connection string"},
-					&cli.StringFlag{Name: "password", Aliases: []string{"p"}, Value: "dev", Usage: "Set the database password in the database connection string"},
-					&cli.StringFlag{Name: "connection", Aliases: []string{"c"}, Value: "5432", Usage: "Set the database port in the database connection string"},
-					&cli.StringFlag{Name: "name", Aliases: []string{"n"}, Value: "studynook", Usage: "Set the database name in the database connection string"},
+					//Running the database and connection
+					&cli.StringFlag{
+						Name:    "address",
+						Aliases: []string{"a"},
+						Value:   ":8080",
+						Usage:   "Set the address to use for the server"},
+					&cli.StringFlag{
+						Name:    "user",
+						Aliases: []string{"u"},
+						Value:   "dev",
+						Usage:   "Set the database username in the database connection string"},
+					&cli.StringFlag{
+						Name:    "password",
+						Aliases: []string{"p"},
+						Value:   "dev",
+						Usage:   "Set the database password in the database connection string"},
+					&cli.StringFlag{
+						Name:    "connection",
+						Aliases: []string{"c"},
+						Value:   "5432",
+						Usage:   "Set the database port in the database connection string"},
+					&cli.StringFlag{
+						Name:    "name",
+						Aliases: []string{"n"},
+						Value:   "studynook",
+						Usage:   "Set the database name in the database connection string"},
+
+					//emailer configs
+					&cli.StringFlag{
+						Name:    "euser",
+						EnvVars: []string{"SMTP_USERNAME"},
+						Usage:   "Set the username in the Emailer configs"},
+					&cli.StringFlag{
+						Name:    "epassword",
+						EnvVars: []string{"SMTP_PASSWORD"},
+						Usage:   "Set the password in the Emailer configs"},
+					&cli.StringFlag{
+						Name:    "eserver",
+						EnvVars: []string{"SMTP_SERVER"},
+						Usage:   "Set the server in the Emailer configs"},
+					&cli.StringFlag{
+						Name:    "eport",
+						EnvVars: []string{"SMTP_PORT"},
+						Usage:   "Set the port in the Emailer configs"},
 				},
 				Action: func(c *cli.Context) error {
 					// address := c.String("address")
@@ -34,6 +74,12 @@ func main() {
 					password := c.String("password")
 					connection := c.String("connection")
 					name := c.String("name")
+
+					//emailer configs
+					euser := c.String("euser")
+					epassword := c.String("epassword")
+					eserver := c.String("eserver")
+					eport := c.String("eport")
 
 					//initializing the database
 					conn, err := db.Connect(user, password, connection, name)
@@ -46,7 +92,8 @@ func main() {
 						fmt.Println(err)
 						return err
 					}
-					controller, err := api.New(database)
+					emailer, err := emails.New(euser, epassword, eserver, eport)
+					controller, err := api.New(database, emailer)
 					log.Fatal(http.ListenAndServe(":8080", controller.Sessions.LoadAndSave(controller.Router)))
 					if err != nil {
 						return err
