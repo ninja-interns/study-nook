a4ce9cc116d30e1a2a488fa9c8a376a7b3f66da5
Joao Soares <62054293+JoaoSoaresDev@users.noreply.github.com>
2021-09-22 13:43:32 +0800

    Updates

--- server/middleware/secure_error.go
+++
@@ -1,8 +0,0 @@
-package middleware
-
-//defining middleware that returns an error
-/*type SecureError func(w http.ResponseWriter, r *http.Request, u *auth.User)
-
-func error_Handler(next SecureError) error {
-
-}*/
