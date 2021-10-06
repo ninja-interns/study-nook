657d53c2afb571d60ed2e0f0d955bbf196d7a627
Alyssa Ninja Linux <aruefenacht14@gmail.com>
2021-10-06 10:54:19 +0800

    .

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { ContextContainer } from "./contexts/ContextContainer";
 import Routes from "./routes/Routes";
 import { ThemeProvider } from "@material-ui/styles";
@@ -12,17 +11,6 @@ function App() {
 			</ContextContainer.Provider>
 		</ThemeProvider>
 	);
-=======
-import { ContextContainer } from "./contexts/ContextContainer"
-import Routes from "./routes/Routes"
-
-function App() {
-	return (
-		<ContextContainer.Provider>
-			<Routes />
-		</ContextContainer.Provider>
-	)
->>>>>>> 6dad207ced2f17e81351ab7e74a379f0d1e35872
 }
 
 export default App;
